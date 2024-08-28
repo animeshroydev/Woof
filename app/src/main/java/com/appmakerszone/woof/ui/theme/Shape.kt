f177820dadf631791d151df055eaed91094d6410
animesh <royanimesh2211@gmail.com>
2024-08-29 01:59:15 +0530

    added shape.kt file

---
+++ app/src/main/java/com/appmakerszone/woof/ui/theme/Shape.kt
@@ -0,0 +1,9 @@
+package com.appmakerszone.woof.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(50.dp),
+)
